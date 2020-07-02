dae5701befed1770373b1ebcdb143b55c42e7ad5
saravanakumarvelayutham <saravanakumar.velayutham@mavs.uta.edu>
2020-07-01 22:34:00 -0500

    added sample files
    modified values from sample project dapp

--- truffle-config.js
+++ truffle-config.js
@@ -8,6 +8,14 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
+    matic: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rpc-mumbai.matic.today`),
+      network_id: 80001,
+      gasPrice: '0x0',
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    }
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
