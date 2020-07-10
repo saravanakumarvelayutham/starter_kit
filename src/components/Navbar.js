57ccd4fbffeaaff1ecf24694e0e3adf9986d511d
saravanakumarvelayutham <saravanakumar.velayutham@mavs.uta.edu>
2020-07-09 22:31:40 -0400

    upd

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,11 +8,11 @@ class Navbar extends Component {
       <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
         <a
           className="navbar-brand col-sm-3 col-md-2 mr-0"
-          href="http://www.dappuniversity.com/bootcamp"
+          href="https://github.com/saravanakumarvelayutham/starter_kit"
           target="_blank"
           rel="noopener noreferrer"
         >
-          EthSwap
+          ShareItSwap
         </a>
 
         <ul className="navbar-nav px-3">
