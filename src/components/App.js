5fe2227c803def1f0bae5894069be992d718aa28
saravanakumarvelayutham <saravanakumar.velayutham@mavs.uta.edu>
2020-07-09 22:51:28 -0400

    upd

--- src/components/App.js
+++ src/components/App.js
@@ -102,6 +102,14 @@ class App extends Component {
     return (
       <div>
         <Navbar account={this.state.account} />
+        <button
+              className="btn btn-light"
+              onClick={(event) => {
+                this.setState({ currentForm: 'swap' })
+              }}
+            >
+            Swap
+        </button>
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '600px' }}>
